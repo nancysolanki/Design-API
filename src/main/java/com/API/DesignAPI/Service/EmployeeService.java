85520fb75839e184dde9b3307dbfae27ed18f7b4
NancySolanki <nnancy.ssolanki@gmail.com>
2024-06-01 21:49:58 +0530

    Adding basic function cass

--- src/main/java/com/API/DesignAPI/Service/EmployeeService.java
+++ src/main/java/com/API/DesignAPI/Service/EmployeeService.java
@@ -1,5 +1,28 @@
 package com.API.DesignAPI.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.API.DesignAPI.Model.Employee;
+import com.API.DesignAPI.Repository.EmployeeRepository;
+import java.util.List;
 
+@Service
 public class EmployeeService {
 
+        @Autowired
+            EmployeeRepository empRepository;  
+            
+            //Create and save new document 
+            public Employee create(Employee emp){
+                return empRepository.save(emp);
+
+            }
+            //Find Employee
+            public List<Employee> findEmployee(Employee emp)
+            {
+                return empRepository.findAll();
+            }
+            //Delete Employee
+            public void deleteEmployee(Employee emp){
+                empRepository.delete(emp);
+            }
 }
